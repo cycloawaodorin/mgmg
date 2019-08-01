9f2a7a529e18e1dd52bea98630031e819a3338d9
cycloawaodorin <cycloawaodorin+github@gmail.com>
2019-08-01 19:25:59 +0900

    fix typo

--- lib/mgmg/const.rb
+++ lib/mgmg/const.rb
@@ -550,7 +550,7 @@ module Mgmg
 			sub_m, sub_s, sub_mc = Equip.__send__(:parse_material, m[3])
 			para = ParamIndex[para]
 			
-			c = ( Equip9[kink][para] * Main9[main_m][para] ).cdiv(100)
+			c = ( Equip9[kind][para] * Main9[main_m][para] ).cdiv(100)
 			new(Mat.v_array(c*Sub9[sub_m][para], c).scalar!(1.quo( main_mc==sub_mc ? 200 : 100 )), kind, (main_s+sub_s).div(2), main_mc, sub_mc)
 		end
 		def compose(main, sub, para)
