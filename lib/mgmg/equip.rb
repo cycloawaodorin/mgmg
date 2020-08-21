e123903dea80494045daf0535ee9dfa4be24b45e
cycloawaodorin <cycloawaodorin+github@gmail.com>
2020-08-22 01:07:39 +0900

    arrange source files

---
+++ lib/mgmg/equip.rb
@@ -0,0 +1,323 @@
+module Mgmg
+	using Refiner
+	class Equip
+		ParamList = %w|攻撃 物防 魔防 HP MP 腕力 器用 素早 魔力|
+		ElementList = %w|火 地 水|
+		EqPosList = %w|武 頭 胴 腕 足 飾|
+		def initialize(kind, weight, star, main_m, sub_m, para, element)
+			@kind, @weight, @star, @main, @sub, @para, @element = kind, weight, star, main_m, sub_m, para, element
+			@total_cost = Vec[0, 0, 0]
+			@history, @min_levels = [self], Hash.new
+		end
+		attr_accessor :kind, :weight, :star, :main, :sub, :para, :element, :total_cost, :history, :min_levels
+		def initialize_copy(other)
+			@kind = other.kind
+			@weight = other.weight
+			@star = other.star
+			@main = other.main
+			@sub = other.sub
+			@para = other.para.dup
+			@element = other.element.dup
+			@total_cost = other.total_cost.dup
+			@history = other.history.dup
+			@min_levels = other.min_levels.dup
+		end
+		
+		def compose(other, level, outsourcing=false)
+			self.class.compose(self, other, level, outsourcing)
+		end
+		
+		def to_s
+			par = @para.map.with_index{|e, i| e==0 ? nil : "#{ParamList[i]}:#{e.comma3}"}.compact
+			elm = @element.map.with_index{|e, i| e==0 ? nil : "#{ElementList[i]}#{e}"}.compact
+			unless elm.empty?
+				par << "EL:#{elm.join('')}"
+			end
+			if @kind == 28
+				ep = @star.map.with_index{|e, i| e==0 ? nil : "#{EqPosList[i]}:#{e}"}.compact
+				"複数装備#{@weight}(#{ep.join(', ')})[#{par.join(', ')}]"
+			else
+				"#{EquipName[@kind]}#{@weight}☆#{@star}(#{MaterialClass[@main]}#{MaterialClass[@sub]})[#{par.join(', ')}]"
+			end
+		end
+		def inspect
+			par = @para.map.with_index{|e, i| "#{ParamList[i]}:#{e}"}
+			par << ( "EL:" + @element.map.with_index{|e, i| "#{ElementList[i]}#{e}"}.join('') )
+			tc = "<コスト:" + @total_cost.map.with_index{|e, i| "#{ElementList[i]}#{e}"}.join('') + '>'
+			if @kind == 28
+				ep = @star.map.with_index{|e, i| "#{EqPosList[i]}:#{e}"}
+				"複数装備#{@weight}(#{ep.join(', ')})[#{par.join(', ')}]#{tc}"
+			else
+				"#{EquipName[@kind]}#{@weight}☆#{@star}(#{MaterialClass[@main]}#{MaterialClass[@sub]})[#{par.join(', ')}]#{tc}"
+			end
+		end
+		
+		def min_level
+			if @kind == 28
+				ret = [0, 0]
+				@min_levels.each do |str, ml|
+					if str.build(-1).kind < 8
+						if ret[0] < ml
+							ret[0] = ml
+						end
+					else
+						if ret[1] < ml
+							ret[1] = ml
+						end
+					end
+				end
+				ret
+			else
+				@min_levels.values.max
+			end
+		end
+		
+		%i|attack phydef magdef hp mp str dex speed magic|.each.with_index do |s, i|
+			define_method(s){ @para[i] }
+		end
+		def atkstr
+			attack()+str()
+		end
+		def atk_sd
+			attack()*2+str()+dex()
+		end
+		def dex_as
+			attack()+str()+dex()*2
+		end
+		def mag_das
+			magic()*4+dex_as()
+		end
+		[:fire, :earth, :water].each.with_index do |s, i|
+			define_method(s){ @element[i] }
+		end
+		
+		def power
+			case @kind
+			when 0, 1
+				atk_sd()*2
+			when 2, 3
+				atkstr()*4
+			when 4
+				[dex_as()*2, mag_das()].max
+			when 5
+				dex_as()*2
+			when 6, 7
+				[magic()*8, atkstr()*4].max
+			when 28
+				(@para.sum*4)-((hp()+mp())*3)
+			else
+				ret = @para.max
+				if ret == magdef()
+					ret*2+magic()
+				else
+					ret*2
+				end
+			end
+		end
+		def magmag
+			magdef()*2+magic()
+		end
+		def fpower
+			if @kind < 8 || @kind == 28
+				power().fdiv(4)
+			else
+				power().fdiv(2)
+			end
+		end
+		
+		def smith_cost(outsourcing=false)
+			if outsourcing
+				if @kind < 8
+					(@star**2)*2+@para.sum+hp().cdiv(4)-hp()+mp().cdiv(4)-mp()
+				else
+					(@star**2)+@para.sum+hp().cdiv(4)-hp()+mp().cdiv(4)-mp()
+				end
+			else
+				if @kind < 8
+					((@star**2)*2+@para.sum+hp().cdiv(4)-hp()+mp().cdiv(4)-mp()).div(2)
+				else
+					((@star**2)+@para.sum+hp().cdiv(4)-hp()+mp().cdiv(4)-mp()).div(2)
+				end
+			end
+		end
+		def comp_cost(outsourcing=false)
+			if outsourcing
+				[(@star**2)*5+@para.sum+hp().cdiv(4)-hp()+mp().cdiv(4)-mp(), 0].max
+			else
+				[((@star**2)*5+@para.sum+hp().cdiv(4)-hp()+mp().cdiv(4)-mp()).div(2), 0].max
+			end
+		end
+		alias :cost :comp_cost
+		
+		def add!(other)
+			if @kind == 28
+				if other.kind == 28
+					@star.add!(other.star)
+				else
+					@star[EquipPosition[other.kind]] += 1
+				end
+			else
+				@star = Vec.new(6, 0)
+				@star[EquipPosition[@kind]] = 1
+				@kind = 28
+				if other.kind == 28
+					@star.add!(other.star)
+				else
+					@star[EquipPosition[other.kind]] += 1
+				end
+			end
+			@weight += other.weight
+			@main = 12
+			@sub = 12
+			@para.add!(other.para)
+			@element.add!(other.element)
+			@total_cost.add!(other.total_cost)
+			@history.concat(other.history)
+			@min_levels.merge!(other.min_levels)
+			self
+		end
+		def +(other)
+			self.dup.add!(other)
+		end
+		def coerce(other)
+			if other == 0
+				zero = self.class.new(28, 0, Vec.new(6, 0), 12, 12, Vec.new(9, 0), Vec.new(3, 0))
+				zero.history.clear
+				[zero, self]
+			else
+				raise TypeError, "Mgmg::Equip can't be coerced into other than 0"
+			end
+		end
+	end
+	
+	class << Equip
+		def build(str, s_level, c_level, left_associative: true)
+			str = Mgmg.check_string(str)
+			stack, str = build_sub0([], str)
+			build_sub(stack, str, s_level, c_level, left_associative)
+		end
+		private def build_sub0(stack, str)
+			SystemEquip.each do |k, v|
+				if Regexp.compile(k).match(str)
+					stack << v
+					str = str.gsub(k, "<#{stack.length-1}>")
+				end
+			end
+			[stack, str]
+		end
+		private def build_sub(stack, str, s_level, c_level, lassoc)
+			if m = /\A(.*\+?)\[([^\[\]]+)\](\+?[^\[]*)\Z/.match(str)
+				stack << build_sub(stack, m[2], s_level, c_level, lassoc)
+				build_sub(stack, "#{m[1]}<#{stack.length-1}>#{m[3]}", s_level, c_level, lassoc)
+			elsif m = ( lassoc ? /\A(.+)\+(.+?)\Z/ : /\A(.+?)\+(.+)\Z/ ).match(str)
+				if c_level < 0
+					compose(build_sub(stack, m[1], s_level, c_level, lassoc), build_sub(stack, m[2], s_level, c_level, lassoc), 0, true)
+				else
+					compose(build_sub(stack, m[1], s_level, c_level, lassoc), build_sub(stack, m[2], s_level, c_level, lassoc), c_level, false)
+				end
+			elsif m = /\A\<(\d+)\>\Z/.match(str)
+				stack[m[1].to_i]
+			else
+				if s_level < 0
+					smith(str, 0, true)
+				else
+					smith(str, s_level, false)
+				end
+			end
+		end
+		
+		def compose(main, sub, level, outsourcing)
+			main_k, sub_k = main.kind, sub.kind
+			main_s, sub_s = main.star, sub.star
+			main_main, sub_main = main.main, sub.main
+			main_sub, sub_sub = main.sub, sub.sub
+			para = Vec.new(9, 0)
+			ele = Vec.new(3, 0)
+			
+			# 9パラメータ
+			coef = Equip9[main_k].dup
+			para[] = coef
+			para.add!(level).e_div!(2)
+			para.e_mul!(sub.para).e_div!(100)
+			coef.sub!(Equip9[sub_k])
+			coef.add!( 100 + (main_s-sub_s)*5 - ( ( main_main==sub_main && main_main != 9 ) ? 30 : 0 ) )
+			coef.add!(Material9[main_main]).sub!(Material9[sub_main])
+			coef.e_mul!(EquipFilter[main_k])
+			para.e_mul!(coef).e_div!( main_k==sub_k ? 200 : 100 )
+			para.add!(main.para)
+			
+			# エレメント
+			ele[] = sub.element
+			ele.e_mul!([75, level].min).e_div!( main_k==sub_k ? 200 : 100 )
+			ele.add!(main.element)
+			
+			ret = new(main_k, main.weight+sub.weight, main_s+sub_s, main_sub, sub_main, para, ele)
+			ret.total_cost.add!(main.total_cost).add!(sub.total_cost)
+			ret.total_cost[1] += ret.comp_cost(outsourcing)
+			ret.min_levels.merge!(main.min_levels, sub.min_levels)
+			ret.history = [*main.history, *sub.history, ret]
+			ret
+		end
+		
+		def smith(str, level, outsourcing)
+			str = Mgmg.check_string(str)
+			unless m = /\A(.+)\((.+\d+),?(.+\d+)\)\Z/.match(str)
+				raise InvalidSmithError.new(str)
+			end
+			kind = EquipIndex[m[1].to_sym]
+			unless kind
+				raise InvalidEquipClassError.new(m[1])
+			end
+			main_m, main_s, main_mc = parse_material(m[2])
+			sub_m, sub_s, sub_mc = parse_material(m[3])
+			para = Vec.new(9, 0)
+			ele = Vec.new(3, 0)
+			
+			# 9パラメータ
+			para[] = Equip9[kind]
+			para.e_mul!(Main9[main_m]).e_div!(100)
+			coef = Sub9[sub_m].dup
+			coef.add!(level)
+			para.e_mul!(coef).e_div!( main_mc==sub_mc ? 200 : 100 )
+			
+			# エレメント
+			ele[] = MainEL[main_m]
+			ele.e_mul!(SubEL[sub_m]).e_div!(6)
+			
+			# 重量
+			weight = ( ( EquipWeight[kind] + SubWeight[sub_m] - level.div(2) ) * ( MainWeight[main_m] ) ).div(10000)
+			
+			ret = new(kind, ( weight<1 ? 1 : weight ), (main_s+sub_s).div(2), main_mc, sub_mc, para, ele)
+			if kind < 8
+				ret.total_cost[0] = ret.smith_cost(outsourcing)
+			else
+				ret.total_cost[2] = ret.smith_cost(outsourcing)
+			end
+			ret.min_levels.store(str, str.min_level)
+			ret
+		end
+		
+		def min_level(str, weight=1)
+			str = Mgmg.check_string(str)
+			unless m = /\A(.+)\((.+\d+),?(.+\d+)\)\Z/.match(str)
+				raise InvalidSmithError.new(str)
+			end
+			kind = EquipIndex[m[1].to_sym]
+			main_m, main_s, = parse_material(m[2])
+			sub_m, sub_s, = parse_material(m[3])
+			
+			q, r = ((weight+1)*10000).divmod(MainWeight[main_m])
+			l = ( EquipWeight[kind] + SubWeight[sub_m] - q + ( r==0 ? 1 : 0 ) )*2
+			[(main_s-1)*3, (sub_s-1)*3, l].max
+		end
+		
+		private def parse_material(str)
+			m = /\A.+?(\d+)\Z/.match(str)
+			mat = MaterialIndex[str.to_sym]
+			if m.nil? || mat.nil?
+				raise InvalidMaterialError.new(str)
+			end
+			[mat, m[1].to_i, mat<90 ? mat.div(10): 9]
+		end
+	end
+end
---
+++ lib/mgmg/utils.rb
@@ -0,0 +1,273 @@
+module Mgmg
+	module Refiner
+		refine Integer do
+			def comma3
+				self.to_s.gsub(/(\d)(?=(\d{3})+(?!\d))/, '\1,')
+			end
+			
+			# 0への丸めを行う整数除
+			def cdiv(other)
+				if self < 0
+					-(-self).div(other)
+				else
+					self.div(other)
+				end
+			end
+		end
+	end
+	using Refiner
+	
+	class InvalidCharacterError < StandardError
+		def initialize(match)
+			@wchar = match[0]
+			super("`#{@wchar}' is not a valid character for recipes")
+		end
+		attr_accessor :wchar
+	end
+	class InvalidBracketError < StandardError; end
+	class InvalidSmithError < StandardError
+		def initialize(str)
+			@recipe = str
+			super("`#{@recipe}' is not a valid recipe for smithing")
+		end
+		attr_accessor :recipe
+	end
+	class InvalidMaterialError < StandardError
+		def initialize(str)
+			@material = str
+			super("`#{@material}' is not a valid material name")
+		end
+		attr_accessor :material
+	end
+	class InvalidEquipClassError < StandardError
+		def initialize(str)
+			@equip = str
+			super("`#{@equip}' is not a valid equip class name")
+		end
+		attr_accessor :equip
+	end
+	
+	CharacterList = /[^\(\)\+0123456789\[\]あきくしすたてなねのびりるイウガクグサジスタダチツデトドニノフブペボムラリルロンヴー一万二光兜典刀剣劣匠双古名吹咆品哮地大天太子安宝小帽弓弩当息悪戦手指斧書服木本杖業樹歴殺水氷法火炎牙物玉王産用界異的皮盾短石砕竜紫綿耳聖脛腕腿般良色衣袋覇質軍軽輝輪重量金鉄鎧闇陽靴額飾首骨鬼龍]/
+	module_function def check_string(str)
+		str = str.gsub(/[\s　\\]/, '')
+		if m = CharacterList.match(str)
+			raise InvalidCharacterError.new(m)
+		end
+		levels = [0, 0]
+		str.each_char do |c|
+			if c == '('
+				if levels[0] == 0
+					levels[0] = 1
+				else
+					raise InvalidBracketError.new("parentheses cannot be nested")
+				end
+			elsif c == ')'
+				if levels[0] == 0
+					raise InvalidBracketError.new("parentheses must be opened before closing")
+				else
+					levels[0] -= 1
+				end
+			elsif c == '['
+				if levels[0] != 0
+					raise InvalidBracketError.new("brackets cannot be nested in parentheses")
+				else
+					levels[1] += 1
+				end
+			elsif c == ']'
+				if levels[0] != 0
+					raise InvalidBracketError.new("parentheses must be closed before closing brackets")
+				elsif levels[1] == 0
+					raise InvalidBracketError.new("brackets must be opened before closing")
+				else
+					levels[1] -= 1
+				end
+			end
+		end
+		if levels[0] != 0
+			raise InvalidBracketError.new("parentheses must be closed")
+		elsif levels[1] != 0
+			raise InvalidBracketError.new("brackets must be closed")
+		end
+		str
+	end
+	
+	class Vec < Array
+		def add!(other)
+			case other
+			when Array
+				self.map!.with_index do |e, i|
+					e + other[i]
+				end
+			else
+				self.map! do |e|
+					e + other
+				end
+			end
+			self
+		end
+		def sub!(other)
+			case other
+			when Array
+				self.map!.with_index do |e, i|
+					e - other[i]
+				end
+			else
+				self.map! do |e|
+					e - other
+				end
+			end
+			self
+		end
+		def e_mul!(other)
+			case other
+			when Array
+				self.map!.with_index do |e, i|
+					e * other[i]
+				end
+			else
+				self.map! do |e|
+					e * other
+				end
+			end
+			self
+		end
+		def e_div!(other)
+			case other
+			when Array
+				self.map!.with_index do |e, i|
+					e.cdiv(other[i])
+				end
+			else
+				self.map! do |e|
+					e.cdiv(other)
+				end
+			end
+			self
+		end
+		def self.irange(size)
+			Vec.new(size, &:itself)
+		end
+		def []=(*arg)
+			case arg.size
+			when 1
+				self.replace(arg[0])
+				arg[0]
+			else
+				super
+			end
+		end
+	end
+	
+	class Mat
+		def initialize(m, n, value=nil)
+			if block_given?
+				@body = Array.new(m) do |i|
+					Array.new(n) do |j|
+						yield(i, j)
+					end
+				end
+			else
+				@body = Array.new(m) do
+					Array.new(n, value)
+				end
+			end
+		end
+		attr_accessor :body
+		def initialize_copy(obj)
+			@body = obj.body.map(&:dup)
+		end
+		def row_size
+			@body.length
+		end
+		def col_size
+			@body[0].length
+		end
+		def each_with_index
+			@body.each.with_index do |row, i|
+				row.each.with_index do |e, j|
+					yield(e, i, j)
+				end
+			end
+			self
+		end
+		def map_with_index!
+			@body.each.with_index do |row, i|
+				row.map!.with_index do |e, j|
+					yield(e, i, j)
+				end
+			end
+			self
+		end
+		def map_with_index(&block)
+			dup.map_with_index!(&block)
+		end
+		def submat_add!(is, js, other)
+			i_s, i_e = index_treatment(is, row_size)
+			j_s, j_e = index_treatment(js, col_size)
+			i_s.upto(i_e).with_index do |i, io|
+				row = @body[i]
+				o_row = other.body[io]
+				j_s.upto(j_e).with_index do |j, jo|
+					row[j] += o_row[jo]
+				end
+			end
+			self
+		end
+		private def index_treatment(idx, max)
+			case idx
+			when Integer
+				[idx, idx]
+			when Range
+				if idx.exclude_end?
+					[idx.first, idx.last-1]
+				else
+					[idx.first, idx.last]
+				end
+			when Array
+				[idx[0], idx[0]+idx[1]-1]
+			when nil
+				[0, max]
+			else
+				raise ArgumentError, "#{idx.class} is not available for Mat index"
+			end.map! do |i|
+				i < 0 ? max-i : i
+			end
+		end
+		def scalar!(value)
+			self.map_with_index! do |e, i, j|
+				e * value
+			end
+		end
+		def scalar(value)
+			self.dup.scalar!(value)
+		end
+		def sum
+			@body.map(&:sum).sum
+		end
+		def pprod(other)
+			r, c = row_size, col_size
+			ret = self.class.new(r+other.row_size-1, c+other.col_size-1, 0)
+			other.each_with_index do |o, i, j|
+				ret.submat_add!(i...(i+r), j...(j+c), scalar(o))
+			end
+			ret
+		end
+		def padd(other)
+			ret = self.class.new([row_size, other.row_size].max, [col_size, other.col_size].max, 0)
+			ret.submat_add!(0...row_size, 0...col_size, self)
+			ret.submat_add!(0...(other.row_size), 0...(other.col_size), other)
+		end
+	end
+	class << Mat
+		def v_array(*ary)
+			new(ary.length, 1) do |i, j|
+				ary[i]
+			end
+		end
+		def h_array(*ary)
+			new(1, ary.length) do |i, j|
+				ary[j]
+			end
+		end
+	end
+end
